e87b4ed0a2956067c507be2a436e3c2718dc5aed
cosonjia <coson.jia@evas.ai>
2023-03-24 10:25:43 +0800

    [fixe] update meta_keys add following keys:lidar2image,velocity,timestamp,token,img_prefix,image_paths

--- mmdet3d/datasets/pipelines/formating.py
+++ mmdet3d/datasets/pipelines/formating.py
@@ -134,13 +134,13 @@ class Collect3D(object):
 
     def __init__(self,
                  keys,
-                 meta_keys=('filename', 'ori_shape', 'img_shape', 'lidar2img',
+                 meta_keys=('filename', 'ori_shape', 'img_shape', 'lidar2img','lidar2image',
                             'depth2img', 'cam2img', 'pad_shape',
                             'scale_factor', 'flip', 'pcd_horizontal_flip',
-                            'pcd_vertical_flip', 'box_mode_3d', 'box_type_3d',
-                            'img_norm_cfg', 'pcd_trans', 'sample_idx',
+                            'pcd_vertical_flip', 'box_mode_3d', 'box_type_3d','velocity',
+                             'img_norm_cfg', 'pcd_trans', 'sample_idx', 'timestamp', 'token',
                             'pcd_scale_factor', 'pcd_rotation', 'pts_filename',
-                            'transformation_3d_flow', 'img_info')):
+                            'transformation_3d_flow', 'img_info','img_prefix', 'image_paths')):
         self.keys = keys
         self.meta_keys = meta_keys
 
